bfc1f33a1d359bce956dad79c66830cb1ed6cd13
mirshafiee <b.mirshafiee90@gmail.com>
2022-09-09 13:47:47 +0200

    use Node to create linkedlist

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,16 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
+    "Creating and linking nodes" example {
+        val node1 = Node(value = 1)
+        val node2 = Node(value = 2)
+        val node3 = Node(value = 3)
+
+        node1.next = node2
+        node2.next = node3
+
+        println(node1)
+    }
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
